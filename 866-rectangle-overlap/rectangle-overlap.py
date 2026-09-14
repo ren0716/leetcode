ec8e2392e11671e5a90982c5a99fdc518467b801
ren0716 <160985050+ren0716@users.noreply.github.com>
2026-09-14 10:55:56 +0800

    Time: 0 ms (100.00%) | Memory: 19.3 MB (59.18%) - LeetSync

--- 866-rectangle-overlap/rectangle-overlap.py
+++ 866-rectangle-overlap/rectangle-overlap.py
@@ -1,22 +1,13 @@
 class Solution:
     def isRectangleOverlap(self, rec1: List[int], rec2: List[int]) -> bool:
-        #check both top left and bottom right for rec1, rec2 and rec2, rec1 respectively
-        #do the same for bottom left and top right
-        rec1_bl = (rec1[0], rec1[1])
-        rec1_tr = (rec1[2], rec1[3])
-        rec2_bl = (rec2[0], rec2[1])
-        rec2_tr = (rec2[2], rec2[3])
-        #derive top left and bottom right
-        rec1_br = (rec1[2], rec1[1])
-        rec1_tl = (rec1[0], rec1[3])
-        rec2_br = (rec2[2], rec2[1])
-        rec2_tl = (rec2[0], rec2[3])
+        # rec = [left, bottom, right, top]
 
-        if rec1_bl[0] < rec2_tr[0] and rec1_bl[1] < rec2_tr[1] and rec1_tr[0] > rec2_bl[0] and rec1_tr[1] > rec2_bl[1]:
-            return True
-        
-        if rec2_bl[0] < rec1_tr[0] and rec2_bl[1] < rec1_tr[1] and rec2_tr[0] > rec1_bl[0] and rec2_tr[1] > rec1_bl[1]:
-            return True
-        
-        return False
+        separated = (
+            rec1[2] <= rec2[0] or  # rec1 is entirely left of rec2
+            rec2[2] <= rec1[0] or  # rec2 is entirely left of rec1
+            rec1[3] <= rec2[1] or  # rec1 is entirely below rec2
+            rec2[3] <= rec1[1]    # rec2 is entirely below rec1
+        )
+
+        return not separated
         
